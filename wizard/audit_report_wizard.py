585ef69b6d83811fcd5819e206f49115c36951b7
Luis Romero <david@css.3par1.com>
2024-07-26 16:13:17 -0400

    Added report totals

--- wizard/audit_report_wizard.py
+++ wizard/audit_report_wizard.py
@@ -1,4 +1,5 @@
 import datetime
+import locale
 
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError, UserError
@@ -14,6 +15,7 @@ class AuditReportWizard(models.TransientModel):
     def generate_report(self):
         current_user_id = self.env.user.id
         tz = pytz.timezone(self.env.user.tz) or pytz.utc
+        locale.setlocale(locale.LC_ALL, self.env.user.lang)
         data = {
             'date': self.date.strftime("%d/%m/%Y"),
             'user_name': None,
@@ -35,22 +37,31 @@ class AuditReportWizard(models.TransientModel):
 
         moves_list = []
 
+        report_total = 0
+        report_total_ref = 0
+
         for move in moves:
             if not move.partner_id:
                 continue
+            amount = move.amount_total_signed
+            amount_ref = next((l.credit_usd for l in move.line_ids if l.credit_usd > 0), 0)
             moves_list.append({
                 'company_name': move.partner_id.name,
                 'memo': move.ref,
-                'amount': f"Bs {move.amount_total_signed}",
-                'amount_ref': f"$ {next((l.credit_usd for l in move.line_ids if l.credit_usd > 0), 0)}"
+                'amount': f"Bs {locale.format('%.2f', round(amount,2), True)}",
+                'amount_ref': f"$ {locale.format('%.2f', round(amount_ref,2), True)}"
             })
+            report_total += amount
+            report_total_ref += amount_ref
 
         data = {
             'date': self.date.strftime("%d/%m/%Y"),
             'journal_name': self.journal_id.name,
             'report_date': pytz.utc.localize(datetime.datetime.now()).astimezone(tz).strftime("%d/%m/%Y %H:%M:%S"),
             'company_name': self.env.company.name,
-            'moves': moves_list
+            'moves': moves_list,
+            'report_total': f"Bs {locale.format('%.2f', round(report_total,2), True)}",
+            'report_total_ref': f"$ {locale.format('%.2f', round(report_total_ref, 2), True)}"
         }
 
         report = self.env.ref('tp1_audit_report.audit_report_action')
